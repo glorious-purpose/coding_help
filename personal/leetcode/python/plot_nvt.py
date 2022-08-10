a7cd4f6ee75df6b11759621eaf6a5c2c4f1f3c9b
Travis Cowart <Travis.Cowart87@gmail.com>
2022-08-10 13:24:03 -0500

    Update: Can now plot multiple test sets with different colors.

--- personal/leetcode/python/plot_nvt.py
+++ personal/leetcode/python/plot_nvt.py
@@ -1,11 +1,14 @@
 import matplotlib.pyplot as plt
 from time import time
 import math
+import numpy as np
 
 
 class Plotter:
     def __init__(self):
         self.time_unit = self.initialize_time()
+        self.mst = 0
+        self.n_values = None
 
     def initialize_time(self):
         times = []
@@ -26,13 +29,19 @@ class Plotter:
     def get_n2_time(self, n_value: int):
         return n_value**2 * self.time_unit
 
-    def plot(self, n_values, result_times, n_times=True, nlogn_times=True, n2_times=True):
+    def plot(self, n_values, result_times, n_times=True, nlogn_times=True, n2_times=True, c="b"):
+        if self.n_values is not None and len(n_values) != self.n_values:
+            raise ValueError(f"{len(n_values)} does not match previous plot's {self.n_values}.")
         indexes = list(range(len(n_values)))
         indexes.sort(key=n_values.__getitem__)
         n_values_sorted = list(map(n_values.__getitem__, indexes))
         result_times_sorted = list(map(result_times.__getitem__, indexes))
+        self.mst = max(self.mst, max(result_times))
 
-        plt.plot(n_values_sorted, result_times_sorted, c="b", label="results")
+        plt.scatter(n_values_sorted, result_times_sorted, c=c, label="results")
+        z = np.polyfit(n_values_sorted, result_times_sorted, 2)
+        p = np.poly1d(z)
+        plt.plot(n_values_sorted, p(n_values_sorted), c=c)
 
         if n_times:
             n_times = [self.get_n_time(x) for x in n_values_sorted]
@@ -46,9 +55,10 @@ class Plotter:
             n2_times = [self.get_n2_time(x) for x in n_values_sorted]
             plt.plot(n_values_sorted, n2_times, c="r", label="n^2")
 
+    def show(self):
         plt.xlabel("N")
         plt.ylabel("Time")
         plt.title("Time Complexity Graph")
         ax = plt.gca()
-        ax.set_ylim([0, max(result_times)])
+        ax.set_ylim([0, self.mst])
         plt.show()
