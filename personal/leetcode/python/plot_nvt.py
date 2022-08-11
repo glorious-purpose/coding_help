6b5771b358c6909641c6ea292bc323c8eab176b4
Travis Cowart <Travis.Cowart87@gmail.com>
2022-08-11 09:52:00 -0500

    Update: Now shows 2^N and N! lines.

--- personal/leetcode/python/plot_nvt.py
+++ personal/leetcode/python/plot_nvt.py
@@ -29,9 +29,17 @@ class Plotter:
     def get_n2_time(self, n_value: int):
         return n_value**2 * self.time_unit
 
-    def plot(self, n_values, result_times, n_times=True, nlogn_times=True, n2_times=True, c="b"):
+    def get_2n_time(self, n_value: int):
+        return 2**n_value * self.time_unit
+
+    def get_nf_time(self, n_value: int):
+        n_value = min(n_value, 170)
+        return math.factorial(n_value) * self.time_unit
+
+    def plot(self, n_values, result_times, plot_n=True, plot_nlogn=True, plot_n2=True, plot_2n=True, plot_nf=True, c="b"):
         if self.n_values is not None and len(n_values) != self.n_values:
             raise ValueError(f"{len(n_values)} does not match previous plot's {self.n_values}.")
+        self.n_values = len(n_values)
         indexes = list(range(len(n_values)))
         indexes.sort(key=n_values.__getitem__)
         n_values_sorted = list(map(n_values.__getitem__, indexes))
@@ -43,17 +51,25 @@ class Plotter:
         p = np.poly1d(z)
         plt.plot(n_values_sorted, p(n_values_sorted), c=c)
 
-        if n_times:
-            n_times = [self.get_n_time(x) for x in n_values_sorted]
-            plt.plot(n_values_sorted, n_times, c="yellow", label="n")
+        if plot_n:
+            calc_times = [self.get_n_time(x) for x in n_values_sorted]
+            plt.plot(n_values_sorted, calc_times, c="yellow", label="n")
 
-        if nlogn_times:
-            nlogn_times = [self.get_nlogn_time(x) for x in n_values_sorted]
-            plt.plot(n_values_sorted, nlogn_times, c="darkorange", label="nlogn")
+        if plot_nlogn:
+            calc_times = [self.get_nlogn_time(x) for x in n_values_sorted]
+            plt.plot(n_values_sorted, calc_times, c="darkorange", label="nlogn")
 
-        if n2_times:
-            n2_times = [self.get_n2_time(x) for x in n_values_sorted]
-            plt.plot(n_values_sorted, n2_times, c="r", label="n^2")
+        if plot_n2:
+            calc_times = [self.get_n2_time(x) for x in n_values_sorted]
+            plt.plot(n_values_sorted, calc_times, c="r", label="n^2")
+
+        if plot_2n:
+            calc_times = [self.get_2n_time(x) for x in n_values_sorted]
+            plt.plot(n_values_sorted, calc_times, c="purple", label="n^2")
+
+        if plot_nf:
+            calc_times = [self.get_nf_time(x) for x in n_values_sorted]
+            plt.plot(n_values_sorted, calc_times, c="darkred", label="n^2")
 
     def show(self):
         plt.xlabel("N")
